f0c11d5b02fd7f258061a016b2783c197388d71e
Ilya Ignatev <silentwasd@gmail.com>
2025-01-01 13:00:10 +0300

    Load first model

--- app/Services/ComposableTable/Searchable.php
+++ app/Services/ComposableTable/Searchable.php
@@ -9,12 +9,14 @@ trait Searchable
     protected function checkSearch(string $column = 'name'): array
     {
         return [
-            $column => 'nullable|string|max:255'
+            $column    => 'nullable|string|max:255',
+            'model_id' => 'nullable'
         ];
     }
 
     protected function applySearch(array $data, Builder $query, string $column = 'name'): Builder
     {
-        return $query->when($data[$column] ?? false, fn(Builder $when) => $when->where($column, 'LIKE', '%' . $data[$column] . '%'));
+        return $query->when($data[$column] ?? false, fn(Builder $when) => $when->where($column, 'LIKE', '%' . $data[$column] . '%'))
+                     ->when($data['model_id'] ?? false, fn(Builder $when) => $when->where('id', $data['model_id']));
     }
 }
