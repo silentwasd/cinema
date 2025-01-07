91811d1426c26908d73bc60d4a1eb21478087041
Ilya Ignatev <silentwasd@gmail.com>
2025-01-07 18:49:08 +0300

    Filesystems update

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,7 +38,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => env('PUBLIC_STORAGE_PATH', storage_path('app/public')),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
